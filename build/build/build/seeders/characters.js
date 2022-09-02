5df96d495c554c6f59198fe98e9ae2d1c7be7c98
Ankara.mg <solroman44@gmail.com>
2022-09-02 04:09:44 -0300

    eso no va!

--- build/build/build/seeders/characters.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.characters = void 0;
-exports.characters = {
-// {
-//     id: string,
-//     name: string,
-// }
-};
