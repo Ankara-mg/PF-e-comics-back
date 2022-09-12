e41658958a23933fd51d0085707d2d27c8dcbc75
Ankara.mg <solroman44@gmail.com>
2022-09-12 01:58:58 -0300

    deploy

--- config/config.js
+++ config/config.js
@@ -1,18 +1,18 @@
 require('dotenv').config();
 
-module.exports = {
+module.exports ={
   "development": {
     "username": "postgres",
-    "host": "localhost",
-    "port": 5432,
-    "database": "ecomics",
+    "host": "containers-us-west-34.railway.app",
+    "port": 6533,
+    "database": "railway",
     "dialect": "postgres",
-    "password" : "alejo10913" 
+    "password" : "InkJ2AcR3c2CwKTBKLgy" 
   },
   "test": {
     "username": "root",
     "password": null,
-    "database": "ecomics",
+    "database": "railway",
     "host": "127.0.0.1",
     "dialect": "postgres"
   },
