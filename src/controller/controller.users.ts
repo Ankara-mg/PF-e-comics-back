6502c83db1a3f2b5205d1c0b5a91f500e8304606
dagoav <dagoav89@gmail.com>
2022-09-14 01:05:52 -0500

    admin routes

--- src/controller/controller.users.ts
+++ src/controller/controller.users.ts
@@ -49,7 +49,7 @@ export const userLogin = async (req: Request, res: Response) => {
         }
         if (user.rol === "USER") {
             const token = jwt.sign({ id: user.id }, secretUser, { expiresIn: 60 * 60 * 24 })
-            res.json({ auth: true, token, Rol: "USER", name: user.username })
+            res.json({ auth: true, token, Rol: "USER", name: user.username, id: user.id })
         }
     } catch (error) {
         console.log(error)
