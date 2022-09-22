52f80115e78769ffb72b3cb5080438d41b3b3df6
Ankara.mg <solroman44@gmail.com>
2022-09-22 04:58:14 -0300

    mail

--- src/controller/controller.mailing.ts
+++ src/controller/controller.mailing.ts
@@ -6,10 +6,13 @@ require('dotenv').config();
 // const apiKey = '49e9caca6b1b3b836f076299d5a84df4e9ab60a1'
 const { transporter } = require("../../config/mail.js")
 
+console.log("TRANSPORTER,", transporter)
+
 
 //----------------------------- http://localhost:3000/comics -----------------------------------
 // 👻
 export const sendMailService = async (email: string[]) => {
+  console.log("TRANSPORTER en funcion", transporter)
   let info = await transporter.sendMail({
     from: 'ecomicsPF@gmail.com', // sender address
     to: [...email], // list of receivers
